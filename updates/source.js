58bf7d5dae4ce7787703bd6b808362165f04d3d8
Sprudeel <leonard@schafer.li>
2021-10-18 00:22:06 +0200

    layout

---
+++ updates/source.js
@@ -0,0 +1,19 @@
+
+GitHubActivity.feed({
+    username: "Sprudeel",
+    selector: "#feed",
+    limit: 15,
+  });
+
+
+GitHubCalendar(".calendar", "Sprudeel");
+    
+// or enable responsive functionality:
+GitHubCalendar(".calendar", "Sprudeel", { responsive: true });
+
+// Use a proxy
+GitHubCalendar(".calendar", "Sprudeel", {
+    proxy (username) {
+    return fetch(`https://your-proxy.com/github?user=${username}`)
+    }
+}).then(r => r.text());
